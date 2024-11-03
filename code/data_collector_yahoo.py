e6b6ef35b7f22e269da7e9b1be1b03facaf660ec
Mr-ESG <17lauyan@gmail.com>
2024-11-03 16:51:56 +0100

    1

---
+++ code/data_collector_yahoo.py
@@ -0,0 +1,32 @@
+import requests
+import yfinance as yf
+import pandas as pd
+import matplotlib.pyplot as plt
+
+tickers = {
+    "SHY": "US (USD)",
+    "XSB.TO": "Canada (CAD)",
+    "IGLS.L": "UK (GBP)",
+    #"XACT-SVERIGE.ST": "Sweden (SEK)",
+    "CSBGC3.SW": "Switzerland (CHF)",
+    "IBGS.MI": "Eurozone (EUR)",
+    "XJSE.DE": "Japan (EUR)"
+}
+
+
+start_date = "2004-01-01"
+end_date = "2024-10-21"
+
+# Download the data
+df = yf.download(list(tickers.keys()), start=start_date, end=end_date, progress=False)
+
+# Convert the data to long format
+df_long = df.stack(['Ticker', 'Price'], future_stack=True).reset_index()
+df_long.rename(columns={'Date': 'date', 'Ticker': 'ticker', 'Price': 'type', 0: 'value'}, inplace=True)
+df_long['date'] = df_long['date'].dt.date
+df_long['ticker'] = df_long['ticker'].map(tickers)
+
+df_long = df_long.dropna(subset=['value'])
+df.to_csv()
+
+print(df_long)
