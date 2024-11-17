ef5578110eb9a4def484db6e3f1be73b96fef249
Mr-ESG <17lauyan@gmail.com>
2024-11-17 22:10:48 +0100

    including missing currencies

--- code/data_collector_yahoo.py
+++ code/data_collector_yahoo.py
@@ -7,7 +7,10 @@ g10_chf_tickers = [
     "USDCHF=X",  # US Dollar
     "CADCHF=X",  # Canadian Dollar
     "SEKCHF=X",  # Swedish Krona
-    "JPYCHF=X"   # Japanese Yen
+    "JPYCHF=X",   # Japanese Yen
+    "AUDCHF=X",  # Australian Dollar
+    "NZDCHF=X",  # New Zealand Dollar
+    "NOKCHF=X"   # Norwegian Krone
 ]
 
 # Define the date range for the data
@@ -19,7 +22,7 @@ df = yf.download(g10_chf_tickers, start=start_date, end=end_date, progress=False
 
 df_long = df.stack(['Ticker', 'Price'], future_stack=True).reset_index()
 df_long.rename(columns={'Date': 'date', 'Ticker': 'ticker', 'Price': 'type', 0: 'value'}, inplace=True)
-df_long['date'] = df_long['date'].dt.date
+df_long['date'] = df_long['date'].dt.strftime('%Y-%m-%d')
 df_long['ticker'] = df_long['ticker'].str.replace('=X', '')
 df_long = df_long.dropna(subset=['value'])
 df_long.to_csv("data/raw/yahoo_FX.csv", index=False)
